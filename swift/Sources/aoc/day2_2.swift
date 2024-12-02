62df3a05c4d998c9cc2109752fde3c0638e44ffb
Andreas Nilsson <codr7@protonmail.com>
2024-12-02 15:39:46 +0100

    Cleaning.

--- swift/Sources/aoc/day2_2.swift
+++ swift/Sources/aoc/day2_2.swift
@@ -1,6 +1,5 @@
 class day2_2: day2_1 {
     override var label: String { "day2:2" }
-    override var path: String { "input2" }
 
     override func isSafe(_ report: Report) -> Bool {
         (0..<report.count).contains(where: 
