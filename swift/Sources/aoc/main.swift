c2654844ed54704b31ca0d5f7a62ccc898928c31
Andreas Nilsson <codr7@protonmail.com>
2024-12-05 07:05:04 +0100

    Cleaning.

--- swift/Sources/aoc/main.swift
+++ swift/Sources/aoc/main.swift
@@ -1,12 +1,12 @@
 try [
-  //day1_1(),
-  //day1_2(),
-  //day2_1(),
-  //day2_2(),
-  //day3_1(),
-  //day3_2(),
-  //day4_1(),
-  //day4_2(),
+  /*day1_1(),
+    day1_2(),
+    day2_1(),
+    day2_2(),
+    day3_1(),
+    day3_2(),
+    day4_1(),
+    day4_2(),*/
   day5_1(),
   day5_2()
 ].forEach({(d: IDay) in try d.run()})
